7ed1971624be745aa85e7018b0f2b0e610c278b9
winfredselwyn <winfredselwyn@gmail.com>
2017-05-04 22:37:48 +0800

    chore: Added folder structure.

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Button, Text } from 'react-native';
+import { Actions } from 'react-native-router-flux';
+
+
+const MainApp = () => {
+  return (
+    <View>
+      <Text>A new view</Text>
+    </View>
+  );
+};
+
+export default MainApp;
